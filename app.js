59dad643051a900db2243eb03cc950fc20bf68c6
Ahmad Alhaji Mustafa <ahmadmg@gmail.com>
2023-05-12 10:26:50 +0200

    add css

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// Test
+
+const fullScreen=document.getElementById("full-screen")
+const mainDiv=document.getElementById("main-div")
+
+fullScreen.addEventListener("click",openFullscreen)
+function openFullscreen() {
+    if (mainDiv.requestFullscreen) {
+        mainDiv.requestFullscreen();
+    } else if (mainDiv.webkitRequestFullscreen) { /* Safari */
+    mainDiv.webkitRequestFullscreen();
+    } else if (mainDiv.msRequestFullscreen) { /* IE11 */
+    mainDiv.msRequestFullscreen();
+    }
+  }
