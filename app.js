c6f697268218357dd837ef29599c4de0002479c8
Ahmad Alhaji Mustafa <ahmadmg@gmail.com>
2023-05-16 13:28:45 +0200

    daily

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-
+// Set a name for each DOM item 
 const fullScreen=document.getElementById("full-screen")
 const mainDiv=document.getElementById("main-div")
 const container=document.getElementById("container")
 const floatBtn=document.getElementById("Float-btn")
+ 
+let colorInterval;
+
 import colors from "./assest.js"
-
+// updating the time on the page
+window.addEventListener("load", (event) => {
+  updateTime()
+});
+// add function to start and end the test
 fullScreen.addEventListener("click",(e)=>openFullscreen(e))
-
 floatBtn.addEventListener("click",endFullScreen)
 
+// F to start the test 
 function openFullscreen() {
     if (mainDiv.requestFullscreen) {
         mainDiv.requestFullscreen();
@@ -17,15 +24,18 @@ function openFullscreen() {
       } else if (mainDiv.msRequestFullscreen) { /* IE11 */
         mainDiv.msRequestFullscreen();
     }
-   
     container.style.visibility="hidden"
-    
     changeBackgroundColor();
-    floatBtn.style.visibility="visible";
-    mainDiv.style.backgroundColor = "#f0f0f0";
+    floatBtn.style.display="block";
+    mainDiv.style.backgroundColor = "transparent";
   }
 
+  
 function endFullScreen(){
+  for( let i=0; i<colors.length; i++){
+    clearTimeout("i")
+  }
+  mainDiv.style.backgroundColor = "transparent";
   if (document.fullscreenElement) {
     document
       .exitFullscreen()
@@ -34,9 +44,9 @@ function endFullScreen(){
   } else {
     document.documentElement.requestFullscreen();
   }
-  floatBtn.style.visibility="hidden";
+  floatBtn.style.display="none";
   container.style.visibility="visible";
-  mainDiv.style.backgroundColor = "";
+ 
 }
 
 
@@ -48,4 +58,11 @@ function changeBackgroundColor() {
       mainDiv.style.backgroundColor = color;
     }, index * 2500); // Change color every 1 second (adjust the interval as needed)
   });
+}
+function updateTime() {
+  let now = new Date();
+  let date = now.toLocaleDateString();
+  let time = now.toLocaleTimeString();
+
+  document.getElementById("datetime").innerHTML =`<h3> ${date}<h3><h3>  ${time}</h3>`
 }
--- assest.js
+++ assest.js
@@ -1,3 +1,6 @@
+// This file is to keep any future data
+
+
 const colors=["#0000FF","#FF0000","#FFFF00","#FF6600","#00FF00", "#6600FF","#000000","#FFFFFF"]
 
 
